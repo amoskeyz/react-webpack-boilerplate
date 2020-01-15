85940d24a00700d151c46854943ec14f56a6e52d
amoskeyz <inspiron.amos@gmail.com>
2020-01-15 13:02:22 +0100

    Setup react-webpack environment

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div>
+        <h1>HELLO WORLD</h1>
+        <h3>Welcome To React-Webpack-Boilerplate</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Main;
